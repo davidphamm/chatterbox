fd68826a6fc5d4f8a24a3570f6f4837a891adb53
davidphamm <92117345+davidphamm@users.noreply.github.com>
2022-07-24 23:00:30 -0700

    Updates

---
+++ precache-manifest.c3d9ca6045c3801fb386e31897889333.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64a9ef4ca87f6ed8c4ae3e4e84c21e7d",
+    "url": "/chatbox/index.html"
+  },
+  {
+    "revision": "403e127056c0f2ecf4ff",
+    "url": "/chatbox/static/css/main.6f76e0ef.chunk.css"
+  },
+  {
+    "revision": "7f49a2738a7819f33f03",
+    "url": "/chatbox/static/js/2.40e78553.chunk.js"
+  },
+  {
+    "revision": "cf4263b26236207b597a32c399243e67",
+    "url": "/chatbox/static/js/2.40e78553.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "403e127056c0f2ecf4ff",
+    "url": "/chatbox/static/js/main.9655633e.chunk.js"
+  },
+  {
+    "revision": "f4e7abd65c3701f97fbd",
+    "url": "/chatbox/static/js/runtime-main.608c4c4e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/chatterbox/precache-manifest.1ffe96476d1d3332da9699739d425e62.js"
+  "/chatbox/precache-manifest.c3d9ca6045c3801fb386e31897889333.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/chatterbox/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/chatbox/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
