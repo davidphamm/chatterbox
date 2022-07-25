b4970fe294f0ab3b4acddb6acc6cb466d5884562
davidphamm <92117345+davidphamm@users.noreply.github.com>
2022-07-24 23:33:39 -0700

    first commit

---
+++ build/precache-manifest.4a92d02d7a4990d61ae2df567b9c6a95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9dbdcb82da8d811f5506ea5a675ea5b",
+    "url": "/chatterbox/index.html"
+  },
+  {
+    "revision": "cd6049f731bd57a1dd8b",
+    "url": "/chatterbox/static/css/main.02ff73d7.chunk.css"
+  },
+  {
+    "revision": "7f49a2738a7819f33f03",
+    "url": "/chatterbox/static/js/2.40e78553.chunk.js"
+  },
+  {
+    "revision": "cf4263b26236207b597a32c399243e67",
+    "url": "/chatterbox/static/js/2.40e78553.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd6049f731bd57a1dd8b",
+    "url": "/chatterbox/static/js/main.9655633e.chunk.js"
+  },
+  {
+    "revision": "fa27366bec35854cc9b5",
+    "url": "/chatterbox/static/js/runtime-main.636d58e8.js"
+  }
+]);
